6e171e17f994ae43b5fcad0b7fead9fd6c5e7eea
chenkangming <1123181523@qq.com>
2022-05-10 12:20:34 +0800

    1.0.2 版本
    1. 创建了 maven 项目
    2. pom.xml 文件新增了打包插件

--- src/main/java/com/jtfr/MainApplication.java
+++ src/main/java/com/jtfr/MainApplication.java
@@ -3,6 +3,6 @@ package com.jtfr;
 public class MainApplication {
 
     public static void main(String[] args) {
-        System.out.println("打包成功");
+        System.out.println("Execute MainApplication successfully");
     }
 }
